ccb31579c75d785224a877610dce0eb43ad483ad
JuJianbang <39935510+JuJianbang@users.noreply.github.com>
2018-06-09 16:05:30 +0800

    Add files via upload

---
+++ LifeCycle/app/src/main/java/edu/fjnu/cse/lifecycle/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.fjnu.cse.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+     @Override
+   protected void onPause() {
+        super.onPause();
+        Log.e("MainActivity","onPause() is invoked！");
+     }
+
+
+    @Override
+         protected void onResume() {
+                super.onResume();
+                 Log.e("MainActivity","onResume() is invoked！");
+             }
+
+
+    @Override
+        protected void onStart() {
+                 super.onStart();
+             Log.e("MainActivity","onStart() is invoked！");
+           }
+
+    @Override
+        protected void onRestart() {
+                super.onRestart();
+               Log.e("MainActivity","onRestart() is invoked！");
+           }
+
+
+    @Override
+        protected void onStop(){
+               super.onStop();
+              Log.e("MainActivity","OnStop() is invoked!");
+           }
+
+}
